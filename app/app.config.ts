82e1cabee979cccb0f72d4989b9b6dc01170d410
victeodoro <victor@victeodoro.com>
2025-12-26 21:14:57 -0300

    feat: config theme colors

---
+++ app/app.config.ts
@@ -0,0 +1,9 @@
+export default defineAppConfig({
+  ui: {
+    colors: {
+      brand: 'brand',
+      accent: 'accent',
+      background: 'background'
+    }
+  }
+})
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,21 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxt/image', '@nuxt/ui']
+  modules: ['@nuxt/content', '@nuxt/image', '@nuxt/ui'],
+  css: ['~/assets/css/main.css'],
+  ui: {
+    theme: {
+      colors: [
+        'brand',
+        'accent',
+        'background',
+        'primary',
+        'secondary',
+        'info',
+        'success',
+        'warning',
+        'error'
+      ]
+    }
+  }
 })
