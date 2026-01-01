1c4cf5fe404447a60a117a469a0be068d5142561
victeodoro <victor@victeodoro.com>
2026-01-01 17:16:49 -0300

    feat: set portuguese as default language

--- app/components/AppGarden/index.vue
+++ app/components/AppGarden/index.vue
@@ -1,8 +1,8 @@
 <template>
     <section id="garden" class="flex flex-col gap-20">
         <div>
-            <h2 class="text-paragraph text-5xl font-serif font-light mb-3">The Garden</h2>
-            <p class="text-paragraph text-2xl font-light">A digital garden is a collection of imperfect notes, essays, and ideas growing slowly over time.</p>
+            <h2 class="text-paragraph text-5xl font-serif font-light mb-3">O Jardim</h2>
+            <p class="text-paragraph text-2xl font-light">Uma coleção de notas e ideias que se desenvolvem lentamente com o tempo.</p>
         </div>
         
         <div class="columns-1 sm:columns-2 lg:columns-3 gap-4">
--- app/components/AppHome/AppHomePresentation.vue
+++ app/components/AppHome/AppHomePresentation.vue
@@ -2,24 +2,23 @@
     <section class="w-[75%]">
 
         <h3 class="text-2xl font-light text-paragraph mb-3">
-            I work with systems and services that break often.
+            Trabalho com sistemas e serviços críticos.
         </h3>
 
         <h1 class="font-serif font-normal text-6xl text-paragraph">
-            <span class="font-bold">Most days</span> are spent learning how 
-            things fail,
+            <span class="font-bold">Em muitos dias</span>, aprendo como as coisas falham.
         </h1>
 
         <h3 class="mt-4 text-2xl font-light text-paragraph">
-            <span class="font-medium">some</span> to exploit their flaws.
+            <span class="font-medium">Em outros</span>, a como explorar e corrigir suas vulnerabilidades.
         </h3>
 
         <h2 class="font-light text-paragraph text-3xl mt-10 mb-3">
-            Pentester, developer, and mediocre singer-songwriter
+            Pentester, desenvolvedor, e cantor-compositor medíocre
         </h2>
 
         <h2 class="font-light text-paragraph text-3xl">
-            Finding flaws in projects where failure is not an option at
+            Corrigindo vulnerabilidades em projetos de escopo nacional na 
             <NuxtLink
                 href="https://patria.org.br/"
                 class="text-brand font-normal underline"
